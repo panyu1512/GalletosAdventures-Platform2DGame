de0dd7e54e14baccb0d87b378514f187c0a5ff10
panyu1512 <kike20ferrer@gmail.com>
2021-04-12 21:01:45 +0200

    Mecánica de deslizo

--- TFG-Videojuegos/Assets/Scripts/Player/PlayerController.cs
+++ TFG-Videojuegos/Assets/Scripts/Player/PlayerController.cs
@@ -14,32 +14,52 @@ public class PlayerController : MonoBehaviour
 
     private Collider2D colliderJugador;
 
-    // Variable que almacena la posición del personaje;
+    // Variable que almacena la posición del suelo;
     public Transform suelo;
+
+    // Variable que almacena la posición de la pared;
+    public Transform pared;
 
     // Variable que nos permitirá decir en que capa queremos que colisione nuestro objeto jugador
     public LayerMask mascaraSuelo;
 
+    // Variable que nos permitirá decir en que capa queremos que colisione nuestro objeto jugador
+    public LayerMask mascaraPared;
+
     public GameObject[] objetos;
 
+    // Varianle que almacenará el objeto con nuestra animación del dash. 
+    public GameObject efectoDash;
 
-    // Variable que almacenará la dirección
+
+    // Variable que almacenará la dirección.
     private float inputDireccionMovimiento;
 
-    // Variable que almacena la velocidad del personaje
+    // Variable que almacena la velocidad del personaje.
     public float velocidadMovimiento = 6.5f;
 
-    // Variable que almacena la fuerza del salto
+    // Variable que almacena la fuerza del salto.
     public float fuerzaSalto = 13.0f;
 
     public float fuerzaMuerte;
 
     private float fuerzaLateral = 10f;
 
+    public float fuerzaDeslizamiento = 0.2f;
+
     // Variable que almacena el radio para comprobar si dentro de dicho radio el personaje colisióna con algún objeto tipo suelo.
-    private float radio = 0.06f;
+    private float radioSuelo = 0.06f;
 
+    // Variable que almacena el radio para comprobar si dentro de dicho radio el personaje colisióna con algún objeto tipo pared.
+    private float radioPared = 0.02f;
 
+    //Variable que almacena el cooldown que tendrá nuestro dash.
+    public float dashEnfriamiento;
+
+    //Variable que almacena la velocidad de nuestro dash.
+    public float fuerzaDash = 30f;
+
+ 
     // Booleano para comprobar si el personaje mira a la izquierda o la derecha
     private bool miraDerecha = true;
 
@@ -60,7 +80,6 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         colliderJugador = GetComponent<Collider2D>();
-
         vivo = true;
 
     }
@@ -72,6 +91,9 @@ public class PlayerController : MonoBehaviour
             ComprobarInput();
             ComprobarDireccionMovimiento();
             actualizarAnimaciones();
+            abrirMenu();
+            checkPared();
+            
         }
     }
 
@@ -79,8 +101,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate() {
         if(vivo == true){
             AplicarMovimiento();
-
-            estaEnSuelo = Physics2D.OverlapCircle(suelo.position, radio, mascaraSuelo);
+            checkSuelo();
+            Dash();
         }
     }
 
@@ -106,7 +128,6 @@ public class PlayerController : MonoBehaviour
     private void AplicarMovimiento(){
         rb.velocity = new Vector2(velocidadMovimiento * inputDireccionMovimiento, rb.velocity.y);
     }
-
     // Función que permitara que el personaje mire a una direccion correspondiente a la que se esta moviendo
     private void ComprobarDireccionMovimiento(){
         if(miraDerecha && inputDireccionMovimiento < 0){
@@ -129,9 +150,50 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0f, 180f, 0f);
     }
 
+    public void checkSuelo(){
+        estaEnSuelo = Physics2D.OverlapCircle(suelo.position, radioSuelo, mascaraSuelo);
+    }
+
+    public void checkPared(){
+        if(Physics2D.OverlapCircle(pared.position, radioPared, mascaraPared) 
+            && Mathf.Abs(inputDireccionMovimiento) > 0 && rb.velocity.y < 0 && !estaEnSuelo){
+                Vector2 v = rb.velocity;
+                v.y = -fuerzaDeslizamiento;
+                rb.velocity = v;
+        }
+    }
+
+    //Función que nos permitirá realizar un dash.
+    private void Dash(){
+
+        dashEnfriamiento -= Time.deltaTime;
+
+        if(Input.GetKey("e") && dashEnfriamiento <=0){
+            GameObject objetoDash;
+            objetoDash = Instantiate(efectoDash, transform.position, transform.rotation);
+
+            if(miraDerecha){
+                rb.AddForce(Vector2.right * fuerzaDash, ForceMode2D.Impulse);
+            }
+            else{
+                rb.AddForce(Vector2.left * fuerzaDash, ForceMode2D.Impulse);
+            }
+
+            dashEnfriamiento = 1.2f;
+            Destroy(objetoDash,1);
+        }        
+    }
+
     // Función que nos permitirá actualizar las animaciones del personaje
     private void actualizarAnimaciones(){
         anim.SetBool("isWalking", estaCaminando);
+    }
+
+    //Función que nos permite abrir el menú en medio de la partida
+    private void abrirMenu(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            SceneManager.LoadScene("SelectLevelMenu");
+        }       
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
