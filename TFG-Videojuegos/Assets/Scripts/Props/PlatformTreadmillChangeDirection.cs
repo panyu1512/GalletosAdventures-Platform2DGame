b91de98a20582e1cbc4ed72862c901ad89a3f97c
panyu1512 <kike20ferrer@gmail.com>
2021-05-03 18:21:58 +0200

    Level 7

--- TFG-Videojuegos/Assets/Scripts/Props/PlatformTreadmillChangeDirection.cs
+++ TFG-Videojuegos/Assets/Scripts/Props/PlatformTreadmillChangeDirection.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformTreadmill : MonoBehaviour
+public class PlatformTreadmillChangeDirection : MonoBehaviour
 {
     public GameObject jugador;
 
     private void OnCollisionEnter2D(Collision2D colision) {
         if(colision.transform.CompareTag("jugador")){
+            jugador.GetComponent<ConstantForce2D>().relativeForce = new Vector2(-200, 0);
             jugador.GetComponent<ConstantForce2D>().enabled = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D colision) {
         if(colision.transform.CompareTag("jugador")){
+            jugador.GetComponent<ConstantForce2D>().relativeForce = new Vector2(200, 0);
             jugador.GetComponent<ConstantForce2D>().enabled = false;
         }
     }
